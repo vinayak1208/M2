5c12dda0577fe204f4ebee261598ade27cfb2561
vinayak1208 <32196479+vinayak1208@users.noreply.github.com>
2017-09-22 18:01:55 +0530

    Create IMobileDAO.java

---
+++ MobileApplication/com.cg.mobile.dao/IMobileDAO.java
@@ -0,0 +1,17 @@
+package com.cg.mobile.dao;
+
+import java.util.ArrayList;
+
+import com.cg.mobile.DTO.MobileDetails;
+import com.cg.mobile.DTO.PurchaseDetails;
+import com.cg.mobile.exception.MobileApplicationException;
+
+public interface IMobileDAO {
+	
+	public int addPurchaseDetails(PurchaseDetails p) throws MobileApplicationException;
+
+	public ArrayList<MobileDetails> retrieveDetails(); 
+
+
+
+}
